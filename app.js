adb809d2c4c4c5c0811a896448ba725a57da836a
jtridat <23510328+jtridat@users.noreply.github.com>
2021-04-06 09:56:43 +0200

    feat(app): add log to database

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
+const db = mongoose.connection;
+
+mongoose.connect('mongodb://' + process.env.DB_LOCALHOST, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
 
 module.exports = app;
