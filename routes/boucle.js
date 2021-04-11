d86de24a451b6a411b3a354cf87b2b89a9651938
jerome tridat <jerometridat@mbp-jt.home>
2021-04-11 12:55:50 +0200

    feat(boucle): add update & delete controllers for boucles
    add update & delete controllers and their routes

--- routes/boucle.js
+++ routes/boucle.js
@@ -6,5 +6,7 @@ const boucleCtrl = require('../controllers/BoucleController.js');
 router.get('/', boucleCtrl.getAllBoucles);
 router.get('/:id', boucleCtrl.getOneBoucle);
 router.post('/', boucleCtrl.addNewBoucle);
+router.put('/:id', boucleCtrl.updateOneBoucle);
+router.delete('/:id', boucleCtrl.deleteOneBoucle);
 
 module.exports = router;
