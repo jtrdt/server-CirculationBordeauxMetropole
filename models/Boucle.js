74d6db89f1e0ad65e0cb7de1e80d9e5aaea4a1de
jtridat <23510328+jtridat@users.noreply.github.com>
2021-04-30 17:03:33 +0200

    feat(boucle schema): add backInService + hasAlert + isStored keys

--- models/Boucle.js
+++ models/Boucle.js
@@ -7,8 +7,8 @@ const boucleSchema = new mongoose.Schema(
       ref: 'User',
       required: true
     },
-    // historique des updates
-    update: [
+    // historique des updates ?
+    updates: [
       {
         by: {
           type: mongoose.Schema.Types.ObjectId,
@@ -23,16 +23,32 @@ const boucleSchema = new mongoose.Schema(
     carfId: { type: String, required: true },
     // zone: { type: String, required: true },
     // crossroad: { type: String, required: true },
-    nature: { type: String, enum: ['CENTRALISE', 'LOCAL'] },
+    nature: { type: String, enum: ['CENTRALISE', 'LOCAL', 'TETRA'] },
     entry: { type: String, required: true },
     label: { type: String, required: true },
     comment: { type: String, required: true },
     recommissioning: { type: Date },
     toPrecise: { type: Boolean, default: false },
     isUrgent: { type: Boolean, default: false },
+    hasAlert: { type: Boolean, default: false },
     sendedDate: { type: Date },
     // qui a archivé + quand
-    backInService: { type: Date }
+    backInService: {
+      date: {
+        type: Date
+      },
+      by: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    },
+    isStored: {
+      date: { type: Date },
+      by: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    }
   },
   { timestamps: true }
 );
