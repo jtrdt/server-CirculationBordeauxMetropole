83ebaf1145f89dd30119ab0cd2dbd3e64f75e1d4
jtridat <23510328+jtridat@users.noreply.github.com>
2021-05-06 12:51:38 +0200

    feat(users' routes): add midleware to check admin status in token

--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const userCtrl = require('../controllers/UserController.js');
+const authAdmin = require('../middlewares/authAdmin.js');
 
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
-router.get('/', userCtrl.getAllUsers);
+router.get('/', authAdmin, userCtrl.getAllUsers);
 
 module.exports = router;
