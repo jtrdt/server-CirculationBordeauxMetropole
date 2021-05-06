ae88c0c302bdba78dbc081aa6b9079f98272440e
jtridat <23510328+jtridat@users.noreply.github.com>
2021-05-06 12:50:03 +0200

    feat(admin middleware): middleware to check admin status in a token

---
+++ middlewares/authAdmin.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET_KEY);
+    const admin = decodedToken.admin;
+    if (!admin) {
+      throw 'Pas les droits nécessaires';
+    } else {
+      next();
+    }
+  } catch (error) {
+    res.status(401).json({ error });
+  }
+};
