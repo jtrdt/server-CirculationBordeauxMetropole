4a7a755dd790ac0769e5169670f9c827918ada88
jtridat <23510328+jtridat@users.noreply.github.com>
2021-05-10 21:41:21 +0200

    feat(user model): add  in schema

--- models/User.js
+++ models/User.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true, unique: true },
+    name: { type: String, required: true, unique: true, trim: true },
+    email: { type: String, required: true, unique: true, trim: true },
     password: { type: String, required: true },
     admin: { type: Boolean, required: true, default: false }
   },
