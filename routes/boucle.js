b73c16ae467f8ff26c4bbf9659ba6806e741a7bb
jtridat <23510328+jtridat@users.noreply.github.com>
2021-05-10 11:04:30 +0200

    feat(auth): add auth middleware

--- routes/boucle.js
+++ routes/boucle.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const boucleCtrl = require('../controllers/BoucleController.js');
+const auth = require('../middlewares/auth.js');
 
 router.get('/', boucleCtrl.getAllBoucles);
 router.get('/:id', boucleCtrl.getOneBoucle);
-router.post('/', boucleCtrl.addNewBoucle);
-router.put('/:id', boucleCtrl.updateOneBoucle);
-router.delete('/:id', boucleCtrl.deleteOneBoucle);
+router.post('/', auth, boucleCtrl.addNewBoucle);
+router.put('/:id', auth, boucleCtrl.updateOneBoucle);
+router.delete('/:id', auth, boucleCtrl.deleteOneBoucle);
 
 module.exports = router;
