0b8e7b4d6e800986ec525adca67920bde8eb33e7
jtridat <23510328+jtridat@users.noreply.github.com>
2021-05-11 21:55:36 +0200

    refactor(bouclectrl): update syntax async

--- controllers/BoucleController.js
+++ controllers/BoucleController.js
@@ -1,49 +1,62 @@
 const Boucle = require('../models/Boucle.js');
 
-exports.getAllBoucles = (req, res) => {
-  Boucle.find()
-    .populate('postedBy', 'name')
-    .populate('backInService.by', 'name')
-    .then(boucle => res.status(200).json(boucle))
-    .catch(error => res.status(400).json({ error }));
-};
-
-exports.getOneBoucle = (req, res) => {
-  const id = req.params.id;
-  Boucle.findById(id)
-    .populate('postedBy', 'name')
-    .populate('backInService.by', 'name')
-    .then(boucle => res.status(200).json(boucle))
-    .catch(error => res.status(400).json({ error }));
-};
-
-exports.addNewBoucle = (req, res) => {
-  const newBoucle = new Boucle({ ...req.body });
-  newBoucle
-    .save()
-    .then(() => {
-      res
-        .set('Location', `/api/boucle/${newBoucle.id}`)
-        .status(201)
-        .send(newBoucle);
-    })
-    .catch(error => res.status(400).json({ error }));
-};
-
-exports.updateOneBoucle = (req, res) => {
-  Boucle.updateOne(
-    { _id: req.params.id },
-    {
-      ...req.body,
-      $push: { update: req.body.update }
+exports.getAllBoucles = async (req, res) => {
+  try {
+    const boucles = await Boucle.find()
+      .populate('postedBy', 'name')
+      .populate('backInService.by', 'name');
+    if (!boucles) {
+      res.status(204);
+      return;
     }
-  )
-    .then(() =>
-      res.status(200).json({
-        message: 'Boucle mise à jour'
-      })
-    )
-    .catch(error => res.status(400).json({ error }));
+    res.status(200).json(boucles);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+};
+
+exports.getOneBoucle = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const boucle = await Boucle.findById(id)
+      .populate('postedBy', 'name')
+      .populate('backInService.by', 'name');
+    if (!boucle) {
+      res.status(204);
+      return;
+    }
+    res.status(200).json(boucle);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+};
+
+exports.addNewBoucle = async (req, res) => {
+  try {
+    const newBoucle = new Boucle({ ...req.body });
+    await newBoucle.save();
+    res
+      .set('Location', `/api/boucle/${newBoucle.id}`)
+      .status(201)
+      .send(newBoucle);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+};
+
+exports.updateOneBoucle = async (req, res) => {
+  try {
+    await Boucle.updateOne(
+      { _id: req.params.id },
+      {
+        ...req.body,
+        $push: { update: req.body.update }
+      }
+    );
+    res.status(200).json({ message: 'Boucle mise à jour' });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 };
 
 /* exports.deleteOneBoucle = (req, res) => {
