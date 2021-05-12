d6ac1176c4cc1207178a7d0e9afc09f1f8c9e315
jtridat <23510328+jtridat@users.noreply.github.com>
2021-05-12 14:34:43 +0200

    refactor(boucle.routes): rename files from import

--- routes/boucle.js
+++ routes/boucle.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const boucleCtrl = require('../controllers/BoucleController.js');
-const auth = require('../middlewares/auth.js');
+const auth = require('../middlewares/authUser.js');
 
 router.get('/', boucleCtrl.getAllBoucles);
-router.get('/:id', boucleCtrl.getOneBoucle);
 router.post('/', auth, boucleCtrl.addNewBoucle);
+router.get('/:id', boucleCtrl.getOneBoucle);
 router.put('/:id', auth, boucleCtrl.updateOneBoucle);
-// router.delete('/:id', auth, boucleCtrl.deleteOneBoucle);
+router.delete('/:id', auth, boucleCtrl.deleteOneBoucle);
 
 module.exports = router;
