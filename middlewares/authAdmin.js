dcfb8b8e04d6362d60d7f679ea65a43a990188c2
jtridat <23510328+jtridat@users.noreply.github.com>
2021-05-12 14:43:00 +0200

    feat(auth middlewares): add a secret algorithm for jwt

--- middlewares/authAdmin.js
+++ middlewares/authAdmin.js
@@ -3,7 +3,9 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET_KEY);
+    const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET_KEY, {
+      algorithms: process.env.TOKEN_ALGO
+    });
     const admin = decodedToken.admin;
     if (!admin) {
       throw 'Pas les droits nécessaires';
