2ac61983f00234f5c190308ce628873172ef7d95
jtridat <23510328+jtridat@users.noreply.github.com>
2021-05-17 18:26:29 +0200

    screen dossier pro

--- models/User.js
+++ models/User.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema(
   {
-    // lowerstring: true
-    name: { type: String, required: true, unique: true, trim: true },
+    firstname: { type: String, required: true, trim: true },
+    lastname: { type: String, required: true, trim: true },
+    name: { type: String, unique: true, required: true, trim: true },
     email: { type: String, required: true, unique: true, trim: true },
-    // theme: { type: String, enum: ['dark', 'light'], default: 'light' },
+    theme: { type: String, enum: ['dark', 'light'], default: 'light' },
     password: { type: String, required: true },
-    admin: { type: Boolean, required: true, default: false },
-    // role: { type: String, enum: ['superAdmin', 'admin', 'user']}
+    admin: { type: Boolean, required: true, default: false }
   },
   { timestamps: true }
 );
