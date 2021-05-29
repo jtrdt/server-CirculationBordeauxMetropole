d0d5b1815257bf5ede4d06575e53e403b953ad4d
jtridat <23510328+jtridat@users.noreply.github.com>
2021-05-29 16:44:17 +0200

    style(boucle's route & model): prettier

--- models/Boucle.js
+++ models/Boucle.js
@@ -8,21 +8,17 @@ const boucleSchema = new mongoose.Schema(
       required: true
     },
     carfId: { type: String, required: true },
-    nature: { type: String, enum: ['CENTRALISE', 'LOCAL', 'TETRA', 'null'] },
+    nature: {
+      type: String,
+      enum: {
+        values: ['CENTRALISE', 'LOCAL', 'TETRA', 'null'],
+        message: '{VALUE} is not supported'
+      }
+    },
     entry: { type: String, required: true },
     label: { type: String, required: true },
     isUrgent: { type: Boolean, default: false },
     comment: { type: String, required: true, trim: true },
-    // comments: [
-    //   {
-    //     by: {
-    //       type: mongoose.Schema.Types.ObjectId,
-    //       ref: 'User'
-    //     },
-    //     content: { type: String, required: true, trim: true },
-    //     date: { type: Date, default: Date.now }
-    //   }
-    // ],
     toPrecise: { type: Boolean, default: false },
     sendedDate: {
       date: { type: Date }
@@ -32,33 +28,13 @@ const boucleSchema = new mongoose.Schema(
       // }
     },
     recommissioning: {
-      date: {
-        type: Date
-      },
-      by: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-      }
+      date: { type: Date },
+      by: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
     },
     isStored: {
       date: { type: Date },
-      by: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-      }
+      by: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
     }
-    // update: [
-    //   {
-    //     by: {
-    //       type: mongoose.Schema.Types.ObjectId,
-    //       ref: 'User'
-    //     },
-    //     date: {
-    //       type: Date,
-    //       default: Date.now
-    //     }
-    //   }
-    // ]
   },
   { timestamps: true }
 );
