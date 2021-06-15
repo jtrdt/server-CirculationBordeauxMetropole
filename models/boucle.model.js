08d1c0ae9f7f67f63d5fbce9725ed400ab613310
jtridat <23510328+jtridat@users.noreply.github.com>
2021-06-15 11:51:03 +0200

    feat(boucle): add comments thread

--- models/boucle.model.js
+++ models/boucle.model.js
@@ -2,11 +2,7 @@ const mongoose = require('mongoose');
 
 const boucleSchema = new mongoose.Schema(
   {
-    postedBy: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true
-    },
+    postedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     carfId: { type: String, required: true },
     nature: {
       type: String,
@@ -19,13 +15,17 @@ const boucleSchema = new mongoose.Schema(
     label: { type: String, required: true },
     isUrgent: { type: Boolean, default: false },
     comment: { type: String, required: true, trim: true },
+    comments: [
+      {
+        by: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+        content: { type: String, trim: true },
+        date: { type: Date, default: Date.now, required: true }
+      }
+    ],
     toPrecise: { type: Boolean, default: false },
     sendedDate: {
-      date: { type: Date }
-      // by: {
-      //   type: mongoose.Schema.Types.ObjectId,
-      //   ref: 'User'
-      // }
+      date: { type: Date },
+      by: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
     },
     recommissioning: {
       date: { type: Date },
