894a49750065a960318e4ad9dbe4b090a3e0e14d
jerome tridat <23510328+jtridat@users.noreply.github.com>
2021-06-24 22:07:44 +0200

    feat(events routes): add middlewares to event's routes

--- routes/event.route.js
+++ routes/event.route.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const eventCtrl = require('../controllers/event.controller.js');
+const auth = require('../middlewares/authUser.js');
 const authModerator = require('../middlewares/authModerator.js');
 
-router.get('/', eventCtrl.getAllEvents);
-router.get('/:id', eventCtrl.getOneEvent);
+router.get('/', auth, eventCtrl.getAllEvents);
+router.get('/:id', auth, eventCtrl.getOneEvent);
 router.post('/', authModerator, eventCtrl.addEvent);
 
 module.exports = router;
