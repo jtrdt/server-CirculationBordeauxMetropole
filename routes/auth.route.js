ce2c0c1f79e77411274c529415e1962edc126eae
kratere <23510328+jtridat@users.noreply.github.com>
2021-07-10 11:47:28 +0200

    feat(auth): add reset + update + forget password functions

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -5,7 +5,10 @@ const authCtrl = require('../controllers/auth.controller.js');
 
 router.post('/signup', authCtrl.signup);
 router.post('/signin', authCtrl.signin);
+router.put('/updatepassword/:id', authCtrl.updatePassword);
 router.get('/confirm/:confirmationCode', authCtrl.verifyUser);
 router.post('/confirm/resend/:id', authCtrl.resendConfirmationCode);
+router.post('/requestresetpassword', authCtrl.requestResetPassword);
+router.put('/resetpassword', authCtrl.updateResetPassword);
 
 module.exports = router;
